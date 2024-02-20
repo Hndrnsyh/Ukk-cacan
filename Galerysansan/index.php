1ab31b58531016bbbb8d21d15b673e28b6fc30d3
SanSan Hendriansyah <156865706+Hndrnsyh@users.noreply.github.com>
2024-02-20 14:13:21 +0700

    Add files via upload

---
+++ Galerysansan/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Website Galeri </title>
+	<link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css">
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="#">Website Galeri </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
+      <div class="navbar-nav me-auto">
+      </div>
+      <a href="register.php" class="btn btn-outline-primary m-1">Daftar</a>
+      <a href="login.php" class="btn btn-outline-primary m-1">Masuk</a>
+    </div>
+  </div>
+</nav>
+    <div class="container mt-3">
+    	<div class="row">
+    				<h2> <font color="#1E90FF" >Selamat Datang di Website Galeri</font> </h2>
+    			</div>
+    		</div>
+    	</div>
+    </div>
+    <footer class="d-flex justify-content-center border-top mt-3
+    bg-primary fixed-bottom">
+    	<p>&copy; Ukk RPL 2024 | SanSan Hendriansyah</p>
+    </footer>
+<script type="text/javascript" src="asset/js/bootstrap.min.js"></script>
+</body>
+</html>
