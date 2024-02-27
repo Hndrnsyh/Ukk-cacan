a9c5b1dd29b8bcc1ceada85f9acb1be0052b1b56
SanSan Hendriansyah <156865706+Hndrnsyh@users.noreply.github.com>
2024-02-27 17:17:57 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Website Galeri </title>
+  <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.min.css">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+    <div class="container">
+      <a class="navbar-brand" href="#">Website Galeri </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse mt-2" id="navbarNavAltMarkup">
+        <div class="navbar-nav me-auto">
+        </div>
+        <a href="register.php" class="btn btn-outline-primary m-1">Daftar</a>
+        <a href="login.php" class="btn btn-outline-primary m-1">Masuk</a>
+      </div>
+    </div>
+  </nav>
+  <div class="container">
+    <div class="row">
+      <div class="card ">
+        <div class="card-body">
+          <div class="card-header ">
+            <marquee scrollamount="20">Selamat Datang di Website Galeri</marquee>
+          </div>
+          <p font size="1" > Website gallery adalah jenis website yang menampilkan konten visual gambar . Biasanya situs ini menyediakan berbagai macam foto dengan kualitas tinggi</font>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <footer class="d-flex justify-content-center border-top mt-3
+    bg-primary fixed-bottom">
+    <p>&copy; Ukk RPL 2024 | SanSan Hendriansyah</p>
+  </footer>
+  <script type="text/javascript" src="asset/js/bootstrap.min.js"></script>
+</body>
+
+</html>
